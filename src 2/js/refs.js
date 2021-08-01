4ba3dc86235c4811d3abb730bb185c0e1a0afa5f
Anthony Tilis <tilissound@gmail.com>
2021-08-01 22:00:54 +0300

    123

---
+++ src 2/js/refs.js
@@ -0,0 +1,10 @@
+export default {
+	bodyRef: document.querySelector('body'),
+	galleryRef: document.querySelector('.gallery'),
+	lightBoxCloseBtnRef: document.querySelector('[data-action="close-lightbox"]'),
+	lightBoxRef: document.querySelector('.js-lightbox'),
+	lightBoxOverlayRef: document.querySelector('.lightbox__overlay'),
+	lightBoxContentRef: document.querySelector('.lightbox__content'),
+	lightBoxImageRef: document.querySelector('.lightbox__image'),
+
+}
